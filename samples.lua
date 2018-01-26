f41c534bb488f804c52dbd49752b3c3d7a7e5f84
Leslie Krause <rkrause@inbox.com>
2018-01-25 22:42:46 -0500

    Build 07
    -- included working code samples for v3.0

--- samples.lua
+++ samples.lua
@@ -1,55 +1,71 @@
+-----------------------------------------------------------------------------------------
 -- How to try this example:
 -- 1) Move this file into a new "afs_test" directory under mods and rename it "init.lua".
--- 2) Create a "depends.txt" file in the new directory with the following lines of text:
+-- 2) Create a "depends.txt" file in the new directory with the following text:
 --		nyancat
 --		formspecs
 -- 3) Launch your Minetest server and enable the "afs_test" mod. Then, login as usual!
+-----------------------------------------------------------------------------------------
 
 minetest.register_privilege( "uptime", "View the uptime of the server interactively" )
 
-local get_nyancat_formspec = function( meta )
-	local uptime = minetest.get_server_uptime( )
-	local formspec = "size[4,4]"
-		.. default.gui_bg_img
-		.. string.format( "label[0.5,0.5;%s %0.1f %s]",
-			minetest.colorize( "#FFFF00", "Server Uptime:" ),
-			meta.is_minutes and uptime / 60 or uptime,
-			meta.is_minutes and "mins" or "secs"
-		)
-		.. "checkbox[0.5,1;is_minutes;Show Minutes;" .. tostring( meta.is_minutes ) .. "]"
-		.. "button[0.5,2;2.5,1;update;Refresh]"
-		.. "button_exit[0.5,3;2.5,1;close;Close]"
-		.. "hidden[view_count;1;number]"
-		.. "hidden[view_limit;5;number]"		-- limit the number of refreshes!
-	return formspec
-end
-
 minetest.override_item( "nyancat:nyancat", {
 	description = "System Monitor",
-        
+
+	can_open = function( pos, player )
+		return minetest.check_player_privs( player, "uptime" )
+	end,
+	before_open = function( pos, node, player )
+		return { is_minutes = false, is_refresh = true, pos = pos }
+	end,
 	on_open = function( meta, player )
 		local player_name = player:get_player_name( )
+		local uptime = minetest.get_server_uptime( )
+		local formspec = "size[4,4]"
+			.. default.gui_bg_img
+			.. string.format( "label[0.5,0.5;%s %0.1f %s]",
+				minetest.colorize( "#FFFF00", "Server Uptime:" ),
+				meta.is_minutes and uptime / 60 or uptime,
+				meta.is_minutes and "mins" or "secs"
+			)
+			.. "checkbox[0.5,1;is_minutes;Show Minutes;" .. tostring( meta.is_minutes ) .. "]"
+			.. "checkbox[0.5,1.5;is_refresh;Auto Refresh;" .. tostring( meta.is_refresh ) .. "]"
+			.. "button_exit[0.5,3;2.5,1;close;Close]"
+			.. "hidden[view_count;1;number]"
+			.. "hidden[view_limit;10;number]"		-- limit the number of refreshes
+			.. "hidden[view_timer;1;number]"		-- auto-refresh every 1 seconds
 
-		if meta.is_minutes == nil then meta.is_minutes = true end
-
-		if minetest.check_player_privs( player, "uptime" ) then
-			return get_nyancat_formspec( meta )
-		else
-                        minetest.chat_send_player( player_name, "Your privileges are insufficient." )
+		return formspec
+	end,
+	after_open = function( fs )
+		if fs.meta.is_refresh == true then
+			fs.start_timer( fs.meta.view_timer )
 		end
 	end,
-	on_close = function( meta, player, fields )
-		if not minetest.check_player_privs( player, "uptime" ) then return end
+	on_close = function( fs, fields )
+		if fields.quit == minetest.FORMSPEC_SIGEXIT then
+			print( "afs_test: Player closed formspec." )
 
-		if meta.view_count == meta.view_limit then
-			minetest.destroy_form( player )
-			print( "afs_test: Player exceeded refresh limit." )
-		elseif fields.update then
-			meta.view_count = meta.view_count + 1
-			minetest.update_form( player, get_nyancat_formspec( meta ) )
+		elseif fields.quit == minetest.FORMSPEC_SIGTIME then
+			if fs.meta.view_count == fs.meta.view_limit then
+				fs.destroy( )
+			else
+				fs.update( )
+				fs.meta.view_count = fs.meta.view_count + 1
+			end
+
 		elseif fields.is_minutes then
-			meta.is_minutes = fields.is_minutes == "true"
-			minetest.update_form( player, get_nyancat_formspec( meta ) )
+			fs.meta.is_minutes = fields.is_minutes == "true"
+			fs.update( )
+			fs.reset_timer( )
+
+		elseif fields.is_refresh then
+			fs.meta.is_refresh = fields.is_refresh == "true"
+			if fs.meta.is_refresh then
+				fs.start_timer( fs.meta.view_timer )
+			else
+				fs.stop_timer( )
+			end
 		end
 	end
 } )
@@ -57,9 +73,8 @@ minetest.override_item( "nyancat:nyancat", {
 minetest.register_chatcommand( "uptime", {
 	description = "View the uptime of the server interactively",
 	func = function( name, param )
-		local get_formspec = function( meta )
+		local on_open = function( meta, player )
 			local uptime = minetest.get_server_uptime( )
-
 			local formspec = "size[4,3]"
 				.. default.gui_bg_img
 				.. string.format( "label[0.5,0.5;%s %0.1f %s]",
@@ -68,21 +83,26 @@ minetest.register_chatcommand( "uptime", {
 					meta.is_minutes and "mins" or "secs"
 				)
 				.. "checkbox[0.5,1;is_minutes;Show Minutes;" .. tostring( meta.is_minutes ) .. "]"
-				.. "button[0.5,2;2.5,1;update;Refresh]"
+				.. "button_exit[0.5,2;2.5,1;close;Close]"
+
 			return formspec
 		end
-		local on_close = function( meta, player, fields )
-			if fields.update then
-				minetest.update_form( player, get_formspec( meta ) )
+		local on_close = function( fs, fields )
+			if fields.quit == minetest.FORMSPEC_SIGEXIT then
+				print( "afs_test: Player closed formspec." )
+
+			elseif fields.quit == minetest.FORMSPEC_SIGTIME then
+				fs.update( )
+
 			elseif fields.is_minutes then
-				meta.is_minutes = fields.is_minutes == "true"
-				minetest.update_form( player, get_formspec( meta ) )
+				fs.meta.is_minutes = fields.is_minutes == "true"
+				fs.update( )
+				fs.reset_timer( )
 			end
 		end
 
-		local player = minetest.get_player_by_name( name )
-		local meta = { is_minutes = false }
-		
-		minetest.create_form( meta, player, get_formspec( meta ), on_close )
+		local fs = FormSession( { is_minutes = false }, name, on_open, on_close )
+
+		fs.start_timer( 1 )
 	end
 } )
